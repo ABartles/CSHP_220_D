c267e737a04c47cee511486e2c9eef019969184a
ABartles <andrew.j.bartles@gmail.com>
2017-03-06 12:31:39 -0800

    Fixing Error

--- HelloWorld/App.xaml.cs
+++ HelloWorld/App.xaml.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 ﻿using System;
 using System.Collections.Generic;
 using System.Configuration;
@@ -16,22 +15,3 @@ namespace HelloWorld
     {
     }
 }
-=======
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
-
-namespace HelloWorld
-{
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : Application
-    {
-    }
-}
->>>>>>> 911c0c11191f37dbe807c2bbc5c1180686407004
