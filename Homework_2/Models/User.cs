cd71c5b98b53f95a523e487c8aa4f4ca1f33527a
ABartles <andrew.j.bartles@gmail.com>
2017-03-09 17:14:38 -0800

    Homework 2

---
+++ Homework_2/Models/User.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_2.Models
+{
+    class User
+    {
+        // local fields 
+        private string name = string.Empty;
+        private string password = string.Empty;
+        private string nameError;
+
+        public override string ToString()
+        {
+            return "Name: " + name + "\tPassword: " + password;
+            
+        }
+
+
+        // Accessor: nameError
+        public string NameError
+        {
+            get
+            {
+                return nameError;
+            }
+            set
+            {
+                if (nameError != value)
+                {
+                    nameError = value;
+                }
+            }
+        }
+
+        // Accessor: name
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                if (name != value)
+                {
+                    name = value;
+                }
+            }
+        }
+
+        // Accessor: password
+        public string Password
+        {
+            get
+            {
+                return password;
+            }
+            set
+            {
+                if (password != value)
+                {
+                    password = value;
+                }
+            }
+        }
+
+        // IDataErrorInfo interface
+        public string Error
+        {
+            get
+            {
+                return NameError;
+            }
+        }
+
+        // IDataErrorInfo interface
+        // Called when ValidatesOnDataErrors=True
+        public string this[string columnName]
+        {
+            get
+            {
+                NameError = "";
+                switch (columnName)
+                {
+                    case "Name":
+                        {
+                            if (string.IsNullOrEmpty(Name))
+                            {
+                                NameError = "Name cannot be empty.";
+                            }
+                            if (Name.Length > 12)
+                            {
+                                NameError = "Name cannot be longer than 12 characters.";
+                            }
+                            break;
+                        }
+                }
+                return NameError;
+            }
+        }
+
+    }
+}
---
+++ Homework_2/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create instance of list 
+            var users = new List<Models.User>();
+
+            // Add items to the newly created list
+            users.Add(new Models.User { Name = "Dave", Password = "hello" });
+            users.Add(new Models.User { Name = "Steve", Password = "steve" });
+            users.Add(new Models.User { Name = "Lisa", Password = "hello" });
+
+            Console.WriteLine("---- Complete List ----");
+
+            foreach (var n in users)
+            {
+                Console.WriteLine("{0}", n);
+            }
+
+            // Display all users with the password of hello
+            Console.WriteLine("\n---- Passwords == hello ----");
+
+            var QueryHello =
+                from Name in users
+                where Name.Password == "hello"
+                select Name;
+            
+            // print to console
+            foreach (var n in QueryHello)
+            {
+                Console.WriteLine("{0}", n);
+            }
+
+
+            // Delete user/passwords where the password is lower(name)
+            Console.WriteLine("\n---- Name.ToLower() != Password ----");
+
+            users.RemoveAll(x => x.Password == x.Name.ToLower());
+
+            // print to console
+            foreach (var n in users)
+            {
+                Console.WriteLine("{0}", n);
+            }
+
+            // Delete the first user with the password == hello
+
+            users.RemoveAt(0);
+
+            // Display the remaining names
+            Console.WriteLine("\n---- Remaining users/password ----");
+
+            // print to console
+            foreach (var n in users)
+            {
+                Console.WriteLine("{0}", n);
+            }
+            
+            // Hold open console
+            Console.ReadLine();
+
+        }
+    }
+}
